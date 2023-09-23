7f80016b058b8762fb7e995ddc927a2c20061586
aokatta <aokatta@163.com>
2023-09-23 23:43:29 +0800

    Update AppConfig.java

--- LiveAnch/app/src/main/java/com/fengtuan/videoanchor/AppConfig.java
+++ LiveAnch/app/src/main/java/com/fengtuan/videoanchor/AppConfig.java
@@ -27,7 +27,7 @@ import java.util.Map;
 
 public class AppConfig {
     //域名
-    public static final String HOST_BASE = "http://api.kuang8.vip";
+    public static final String HOST_BASE = "http://api.xiaoxiong6767.xyz";
 //    public static final String HOST_BASE = "http://" + HOST_BASE;
     public static final String HOST(){
         if (AppConfig.getInstance().getConfigIns() != null){
